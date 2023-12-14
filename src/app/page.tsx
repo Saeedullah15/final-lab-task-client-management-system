2c2ace43890912b36b7a25100254bb5c176ee9e0
Saeedullah15 <saeedullahazim65@gmail.com>
2023-12-14 15:20:45 +0600

    login page added

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center">Welcome to Client Management System!</h1>
+    </div>
+  )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small>Client Management System. All Rights Reserved.</small></p>
+        </div>
+    );
+};
+
+export default Footer;
